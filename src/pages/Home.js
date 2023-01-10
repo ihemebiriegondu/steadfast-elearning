71d68e94b505a3f2dbbfab7d5d42260cb84b17d9
ihemebiriegondu <ihemebiriegondu@gmail.com>
2023-01-10 22:27:27 +0100

    added dashboard

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,22 @@
 import React from 'react'
+import userImg from '../assets/user-icon.png'
+import MyCards from '../components/MyCards'
+import '../css/home.css'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className='dashboard'>
+      <div className='top-nav d-flex justify-content-between align-items-baseline'>
+        <h5>Hello, <span>Egondu</span></h5>
+        <img src={userImg} alt='' />
+      </div>
+      <div className='middle-div'>
+        <div className='row row-cols-lg-3 row-cols-md-2 row-cols-1 justify-content-center g-4'>
+          <MyCards cardTitle="Practice Questions" />
+          <MyCards cardTitle="Take an Exam"  />
+        </div>
+      </div>
+    </div>
   )
 }
 
