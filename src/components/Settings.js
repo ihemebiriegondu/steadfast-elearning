a6d5a697a67cd46553b5353ef434a4041034b81b
ihemebiriegondu <ihemebiriegondu@gmail.com>
2023-01-16 22:37:55 +0100

    added function to update profile picture

--- src/components/Settings.js
+++ src/components/Settings.js
@@ -1,14 +1,73 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useUserAuth } from '../context/UserAuthContext'
+import { updateProfile } from 'firebase/auth'
+import { auth } from "../firebase";
+import { storage } from '../firebase';
+import { ref, uploadString, getDownloadURL } from "firebase/storage";
 import "../css/settings.css"
 import { BiPlus } from 'react-icons/bi'
+import { BsFillExclamationCircleFill } from 'react-icons/bs'
+import { GiCheckMark } from 'react-icons/gi'
+import { Alert } from 'react-bootstrap';
 
 const Settings = () => {
 
     const { user } = useUserAuth();
+    const [error, setError] = useState('');
+    const [success, setSuccess] = useState('');
+
+    //alerts
+    const alertDiv = document.querySelectorAll(".error-div")
+
+    const hideAlertFunction = () => {
+        alertDiv.forEach(alert => {
+            alert.classList.add("d-none")
+        });
+    }
 
     const changeProfilePhoto = () => {
-        
+
+        //getting image file and changing the src for the profile picture updating
+        let imgInput = document.getElementById("profilepic")
+        let displayImg = document.getElementById("displayPicture")
+        let reader = new FileReader();
+        let imgInputFile = imgInput.files[0]
+        let imgInputFileName = imgInputFile.name
+
+        //console.log(imgInputFileName)
+        reader.readAsDataURL(imgInputFile);
+        //console.log(imgInput.files[0]);
+        reader.onload = () => {
+            displayImg.setAttribute("src", reader.result)
+            //getting images
+            const storageRef = ref(storage, `images/${imgInputFileName}`);
+
+            uploadString(storageRef, reader.result, 'data_url').then(
+                (snapshot) => {
+                    //console.log('Uploaded a data_url string!');
+                    getDownloadURL(snapshot.ref).then((link) => {
+                        displayImg.setAttribute("src", link)
+
+                        updateProfile(auth.currentUser, {
+                            photoURL: link
+                        }).then(() => {
+                            alertDiv.forEach(alert => {
+                                alert.classList.remove("d-none")
+                            });
+
+                            setSuccess("Updated successfully")
+                            setTimeout(hideAlertFunction, 5000)
+                        }).catch((error) => {
+                            alertDiv.forEach(alert => {
+                                alert.classList.remove("d-none")
+                            });
+
+                            setError(error.code)
+                            setTimeout(hideAlertFunction, 5000)
+                        });
+                    })
+                })
+        }
     }
 
     return (
@@ -17,16 +76,23 @@ const Settings = () => {
                 <div className='top-setting d-flex justify-content-center align-items-center py-4 px-4'>
                     <div className='d-flex flex-column align-items-center'>
                         <div className='img-div mb-2'>
-                            <img src={user.photoURL} alt=''></img>
+                            <img src={user.photoURL} id='displayPicture' alt=''></img>
                         </div>
                         <div className='user-info-div'>
                             <h5 className='text-center pb-0 mb-0'>{user.displayName}</h5>
                             <p className='text-center'>{user.email}</p>
                         </div>
-                        <div className='btn-div'>
-                            <button>Change photo <BiPlus className='mb-1' onClick={changeProfilePhoto()} /></button>
+                        <div className='btn-div d-flex flex-column justify-content-center align-items-center'>
+                            <input type="file" accept="image/*" id="profilepic" name='profilepic' onChange={() => changeProfilePhoto()} className="d-none" />
+                            <label htmlFor='profilepic' className='d-inline-block text-center'>Change photo <BiPlus className='mb-1' /></label>
                         </div>
                     </div>
+                </div>
+                <div className='error-div'>
+                    {error && <Alert variant='danger' className='error'><BsFillExclamationCircleFill className='text-danger me-4 fs-4' />{error}</Alert>}
+                </div>
+                <div className='error-div'>
+                    {success && <Alert variant='success' className='error'><GiCheckMark className='text-success me-4 fs-4' />{success}</Alert>}
                 </div>
             </div>
         </div>
--- src/firebase.js
+++ src/firebase.js
@@ -2,6 +2,7 @@
 import { initializeApp } from "firebase/app";
 //import { getAnalytics } from "firebase/analytics";
 import { getAuth } from 'firebase/auth'
+import { getStorage } from "firebase/storage";
 
 
 // TODO: Add SDKs for Firebase products that you want to use
@@ -10,18 +11,19 @@ import { getAuth } from 'firebase/auth'
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyA-PFbpldT06LHV7UuaXub3S0M5Hun9LZY",
-    authDomain: "steadfastprivateschool.firebaseapp.com",
-    projectId: "steadfastprivateschool",
-    storageBucket: "steadfastprivateschool.appspot.com",
-    messagingSenderId: "571984421647",
-    appId: "1:571984421647:web:86828fd7a22aa31ef6547a",
-    measurementId: "G-8WTZSTF3D8"
-  };
+  apiKey: "AIzaSyA-PFbpldT06LHV7UuaXub3S0M5Hun9LZY",
+  authDomain: "steadfastprivateschool.firebaseapp.com",
+  projectId: "steadfastprivateschool",
+  storageBucket: "steadfastprivateschool.appspot.com",
+  messagingSenderId: "571984421647",
+  appId: "1:571984421647:web:86828fd7a22aa31ef6547a",
+  measurementId: "G-8WTZSTF3D8"
+};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
+export const storage = getStorage(app)
 
 //const analytics = getAnalytics(app);
 
