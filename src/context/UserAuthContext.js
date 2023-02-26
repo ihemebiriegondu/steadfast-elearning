ede4c9e1bebab0ffdb1bac213adb0f4b05a3fce0
ihemebiriegondu <ihemebiriegondu@gmail.com>
2023-02-26 21:44:33 +0100

    updated reset link

--- src/context/UserAuthContext.js
+++ src/context/UserAuthContext.js
@@ -120,7 +120,7 @@ export function UserAuthContextProvider({ children }) {
 
     function forgetpassword(email) {
         return sendPasswordResetEmail(auth, email, {
-            url: 'http://localhost:3000/newpassword'
+            url: 'https://steadfastprivateschool-elearning.netlify.app/newpassword'
         });
     }
 
